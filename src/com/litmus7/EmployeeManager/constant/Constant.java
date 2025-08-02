3e1139c0383a66e457b0ff582cd09b9867853476
Malikk077 <167957940+Malikk077@users.noreply.github.com>
2025-08-02 16:53:37 +0530

    Update Constant.java

--- src/com/litmus7/EmployeeManager/constant/Constant.java
+++ src/com/litmus7/EmployeeManager/constant/Constant.java
@@ -3,21 +3,13 @@ package com.litmus7.EmployeeManager.constant;
 import java.util.HashMap;
 
 public class Constant {
-	public static final String insertToemployees="INSERT INTO employees (emp_id, first_name, last_name, email, phone, department, salary, join_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-	public static final String select1WithEmployeeId="select 1 from employees where emp_id = ?";
-	public static final String selectAllFromEmployee="select * From Employees";
-    public static final HashMap<Integer, String>  errorCodeMap = new HashMap<>();
-    static {     
-        errorCodeMap.put(200, "Operation Successful");
-        errorCodeMap.put(400, "Bad Request");
-        errorCodeMap.put(404, "Not Found");
-        errorCodeMap.put(500, "Internal Server Error");
-        errorCodeMap.put(501, "Provided file is not a CSV");
-        errorCodeMap.put(502, "File path is missing");
-        errorCodeMap.put(505, "error writting file");
-        errorCodeMap.put(206, "Partial success: some records failed to insert.");
-        errorCodeMap.put(506, "Data write failed completely.");
-        
+	public static final int SUCCESS = 200;
+	public static final int FAILURE = 500;
+	public static final int FILE_PATH_MISSING = 502;
+	public static final int FILE_NOT_CSV = 501;
+	public static final int PARTIAL_SUCCESS = 207;
+	
+	
     }
 	
 	

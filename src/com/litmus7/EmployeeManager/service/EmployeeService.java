dadf635d028fd13a1d0e2dc92306642810debaef
Malikk077 <167957940+Malikk077@users.noreply.github.com>
2025-08-02 16:39:48 +0530

    Update EmployeeService.java

--- src/com/litmus7/EmployeeManager/service/EmployeeService.java
+++ src/com/litmus7/EmployeeManager/service/EmployeeService.java
@@ -1,50 +1,72 @@
 package com.litmus7.EmployeeManager.service;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.litmus7.EmployeeManager.util.CsvUtil;
 import com.litmus7.EmployeeManager.util.ValidationUtil;
 import com.litmus7.EmployeeManager.Dto.Employees;
 import com.litmus7.EmployeeManager.dao.EmployeeDao;
 
 public class EmployeeService {
-	public  boolean writeToDb(String[] values) 
+	public Map<String, Integer> writeToDb(String file) 
 	{
+		Map<String, Integer> count=new HashMap<>();    
+		count.put("total", 0);
+		count.put("success", 0);
+		count.put("failure", 0);
 		
-		Employees employee =new Employees();
-		try
-		{
-			employee.setEmployeeId(Integer.parseInt(values[0].trim()));
-		    employee.setFirstName(values[1].trim());
-		    employee.setLastName(values[2].trim());
-		    employee.setEmail(values[3].trim());
-		    employee.setPhone(values[4].trim());
-		    employee.setDepartment(values[5].trim());
-		    employee.setSalary(Double.parseDouble(values[6].trim()));
+		
+		List<String[]> records = CsvUtil.readCSV(file);
+        
+		for (String[] values : records) 
+        {
+			count.put("total", count.get("total") + 1);
+        	Employees employee =new Employees();
+    		try
+    		{
+    			employee.setEmployeeId(Integer.parseInt(values[0].trim()));
+    		    employee.setFirstName(values[1].trim());
+    		    employee.setLastName(values[2].trim());
+    		    employee.setEmail(values[3].trim());
+    		    employee.setPhone(values[4].trim());
+    		    employee.setDepartment(values[5].trim());
+    		    employee.setSalary(Double.parseDouble(values[6].trim()));
 
-		    
-		    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		    employee.setJoinDate(sdf.parse(values[7].trim()));		
-		 }
-		catch(Exception e)
-		{
-			e.printStackTrace();
-			return false;
-			
-		}
-		if (!ValidationUtil.validateEmployee(employee)) return false;
-		
-		if (EmployeeDao.doesEmployeeExist(employee.getEmployeeId())) return false;
-		
-		boolean emp =EmployeeDao.storeInDB(employee) ;
-		return emp;
-	 }
+    		    
+    		    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    		    employee.setJoinDate(sdf.parse(values[7].trim()));		
+    		 }
+    		catch(Exception e)
+    		{
+    			e.printStackTrace();
+    			count.put("failure", count.get("failure") + 1);
+    			continue;
+    		}
+    		if (!ValidationUtil.validateEmployee(employee)){
+    			count.put("failure", count.get("failure") + 1);
+    			continue;
+    		}
+    			
+    		if (EmployeeDao.doesEmployeeExist(employee.getEmployeeId())){
+    			count.put("failure", count.get("failure") + 1);
+    			continue;
+    		}
+    			
+    		if(EmployeeDao.storeInDB(employee)) {
+    			count.put("success", count.get("success") + 1);
+    		}
+    	}
+		return count;
+	}
+    			
+    		
+    		
 
 	public List<Employees> readAllFromDb() 
 	{
-		List<Employees> employeeList =new ArrayList<>();
-		employeeList=EmployeeDao.selectAllEmployees();
-		return employeeList;
+		return EmployeeDao.selectAllEmployees();
 	}
 
 }

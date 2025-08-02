bd4991be395e879646207cf15ad850fa5aebb622
Malikk077 <167957940+Malikk077@users.noreply.github.com>
2025-08-02 16:37:22 +0530

    Update EmployeeManagerClient.java

--- src/com/litmus7/EmployeeManager/UI/EmployeeManagerClient.java
+++ src/com/litmus7/EmployeeManager/UI/EmployeeManagerClient.java
@@ -1,38 +1,40 @@
 package com.litmus7.EmployeeManager.UI;
-
 import java.util.List;
-
 import com.litmus7.EmployeeManager.Controller.EmployeeManagerController;
 import com.litmus7.EmployeeManager.Dto.Employees;
 import com.litmus7.EmployeeManager.Dto.Response;
-import com.litmus7.EmployeeManager.constant.Constant;
+
+
 
 public class EmployeeManagerClient {
 
 	public static void main(String[] args) {
 		EmployeeManagerController controller = new EmployeeManagerController();
 		
-		Response<Integer> result = controller.writeDataToDb("src/com/litmus7/EmployeeManager/Dto/employees.csv");
-		if (result.getStatusCode()==200) 
+		Response<Integer> response = controller.writeDataToDb("src/com/litmus7/EmployeeManager/Dto/employees.csv");
+		if (response.getStatusCode() == 200) {
+            System.out.println("All records were inserted succesfully.");
+            System.out.println("Rows inserted: "+ response.getData());
+        } else if (response.getStatusCode() == 207) {
+            System.out.println("Message: " + response.getErrorMessage());
+            System.out.println("Inserted Count: " + response.getData());
+        } else {
+            System.out.println("Message: " + response.getErrorMessage());
+        }
+		
+		Response<List<Employees>> employeeListResponse=controller.getAllEmployees();
+		if (employeeListResponse.getStatusCode()==200) 
 		{
-			System.out.println(result.getStatusCode()+"  "+Constant.errorCodeMap.get(result.getStatusCode())+"  No of rows inserted:"+result.getData());
+			for (Employees emp:employeeListResponse.getData())
+			{
+				System.out.println(emp.toDetailedString());
+		    }
 		}
 		else {
-			System.out.println(result.getStatusCode()+"  "+Constant.errorCodeMap.get(result.getStatusCode())+"  No of rows inserted:"+result.getData());
+			System.out.println("Message: " + employeeListResponse.getErrorMessage());
 		}
-		
-//		Response<List<Employees>> employeeListResponse=controller.getAllEmployees();
-//		if (employeeListResponse.getStatusCode()==200) 
-//		{
-//			for (Employees emp:employeeListResponse.getData())
-//			{
-//				System.out.println(emp.toDetailedString());
-//		    }
-//		}
-//		else {
-//			System.out.println(employeeListResponse.getStatusCode()+"  "+Constant.errorCodeMap.get(employeeListResponse.getStatusCode())+"  "+employeeListResponse);
-//		}
 		
 	}
 
 }
+

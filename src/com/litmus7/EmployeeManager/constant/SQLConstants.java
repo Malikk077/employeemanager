2d5b1a3d1bc102fb751d9fa5aefecee8bbe08abd
Malikk077 <167957940+Malikk077@users.noreply.github.com>
2025-08-02 16:50:47 +0530

    Update SQLConstants.java

--- src/com/litmus7/EmployeeManager/constant/SQLConstants.java
+++ src/com/litmus7/EmployeeManager/constant/SQLConstants.java
@@ -2,6 +2,6 @@ package com.litmus7.employeemanager.constants;
 
 public class SQLConstants {
   public static final String INSERT_TO_EMPLOYEES="INSERT INTO employees (emp_id, first_name, last_name, email, phone, department, salary, join_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-	public static final String SELECT_1_FROM_EMPLOYEE="select 1 from employees where emp_id = ?";
-	public static final String SELECT_ALL_EMPLOYEE="select * From Employees";
+	public static final String SELECT_1_FROM_EMPLOYEES="select 1 from employees where emp_id = ?";
+	public static final String SELECT_ALL_EMPLOYEES="select * From Employees";
 }
